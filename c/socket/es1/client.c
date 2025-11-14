ce9c10a9e099daa4c7c6238158906d45fd98c695
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2025-11-14 17:04:11 +0100

    DONE c/socket/es1

--- c/socket/es1/client.c
+++ c/socket/es1/client.c
@@ -1,32 +1,59 @@
-#include <stdio.h>      //std in-out
-#include <stdlib.h>     //per utilizzo di certe funzioni:htonl,rand,....
-#include <sys/socket.h> //funz. accept+bind+listen
-#include <sys/types.h>  //funz. accept
-#include <netinet/in.h> //definiscono la struttura degli indirizzi
-#include <string.h>     //funz. stringhe
-#include <errno.h>      //gestioni errori connessione
-#include <ctype.h>      //bind
-#include <unistd.h>     //file header che consente l'accesso alle API dello standard POSIX
+#include <stdio.h>      // std in-out
+#include <stdlib.h>     // per utilizzo di certe funzioni:htonl,rand,....
+#include <sys/socket.h> // funz. accept+bind+listen
+#include <sys/types.h>  // funz. accept
+#include <netinet/in.h> // definiscono la struttura degli indirizzi
+#include <string.h>     // funz. stringhe
+#include <errno.h>      // gestioni errori connessione
+#include <ctype.h>      // bind
+#include <unistd.h>     // file header che consente l'accesso alle API dello standard POSIX
 
 
-#define Serverport 1450
+#define SERVER_PORT 1500
 
 
-int main() {
-    struct sockaddr_in servizio;
+int main(int argc, char *argv[]) {
+    struct sockaddr_in destination;
+
+    // Define the destination
+    destination.sin_family = AF_INET;
+    destination.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    destination.sin_port = htons(SERVER_PORT);
     
-    servizio.sin_family = AF_INET;
-    servizio.sin_addr.s_addr = htonl("192.168.60.162"); // Converte l'indirizzo in bit
-    servizio.sin_port = htons(Serverport);
-    
-    char stringa[20];
+    char stringa[20] = "";
     int socketfd;
-    socketfd = socket(AF_INET, SOCK_STREAM, 0);
-    connect(socketfd, (struct sockaddr*)&servizio, sizeof(servizio));
-    
-    printf("Inserisci la stringa\n");
-    scanf("%s", stringa);
-    
-    write(socketfd, stringa, sizeof(stringa));
-    close(socketfd);
+
+    // Enter the main loop
+    // Every cycle: 
+    // - create a socket
+    // - establish a connection with the server
+    // - send the message
+    // - close the socket
+    // NB: if the user insert a "q" this program finishes
+    while (1) {
+        // Create the socket
+        socketfd = socket(AF_INET, SOCK_STREAM, 0);
+        
+        // Create a connection with the destination
+        connect(socketfd, (struct sockaddr*)&destination, sizeof(destination));
+
+        printf("\nInserisci stringa: ");
+        scanf("%s", stringa);
+
+        if (strcmp(stringa, "q") == 0) {
+            printf("Closing the client\n");
+            close(socketfd);
+            break;
+        }
+
+        write(socketfd, stringa, sizeof(stringa));
+        read(socketfd, stringa, sizeof(stringa));
+
+        printf("Server response: %s", stringa);
+
+        close(socketfd);
+    }
+
+    printf("\n\n");
+    return 0;
 }
--- c/socket/es1/server.c
+++ c/socket/es1/server.c
@@ -1,39 +1,51 @@
-#include <stdio.h>      //std in-out
-#include <stdlib.h>     //per utilizzo di certe funzioni:htonl,rand,....
-#include <sys/socket.h> //funz. accept+bind+listen
-#include <sys/types.h>  //funz. accept
-#include <netinet/in.h> //definiscono la struttura degli indirizzi
-#include <string.h>     //funz. stringhe
-#include <errno.h>      //gestioni errori connessione
-#include <ctype.h>      //bind
-#include <unistd.h>     //file header che consente l'accesso alle API dello standard POSIX
+#include <stdio.h>      // std in-out
+#include <stdlib.h>     // per utilizzo di certe funzioni:htonl,rand,....
+#include <sys/socket.h> // funz. accept+bind+listen
+#include <sys/types.h>  // funz. accept
+#include <netinet/in.h> // definiscono la struttura degli indirizzi
+#include <string.h>     // funz. stringhe
+#include <errno.h>      // gestioni errori connessione
+#include <ctype.h>      // bind
+#include <unistd.h>     // file header che consente l'accesso alle API dello standard POSIX
 
 
-#define Serverport 1450
+#define SERVER_PORT 1500
 
 
-int main() {
-    struct sockaddr_in servizio;
+int main(int argc, char *argv[]) {
+    struct sockaddr_in destination;
 
-    servizio.sin_family = AF_INET;
-    servizio.sin_addr.s_addr = htonl(INADDR_ANY); // Converte l'indirizzo in bit
-    servizio.sin_port = htons(Serverport);
+    destination.sin_family = AF_INET;
+    destination.sin_addr.s_addr = htonl(INADDR_ANY);
+    destination.sin_port = htons(SERVER_PORT);
 
-    char stringa[20];
-    int socketfd, soa, fromlen = sizeof(servizio);
+    int socketfd, soa, destlen = sizeof(destination);  
     socketfd = socket(AF_INET, SOCK_STREAM, 0);
-    bind(socketfd, (struct sockaddr_in*)&servizio, sizeof(servizio));
+    bind(socketfd, (struct sockaddr*)&destination, sizeof(destination));
     listen(socketfd, 10);
 
-    // Ciclo infinito
-    for (;;) {
+    char message[20] = "";
+    char msg_received[17] = "MESSAGE RECEIVED\0";
+    // Enter the main loop:
+    // - accept a connection
+    // - read the message from the soa
+    // - print the message
+    // - close the soa
+    while (1) {
         printf("Server in ascolto ...\n");
         fflush(stdout);
 
-        soa = accept(socketfd, (struct sockaddr_in*)&servizio, &fromlen);
-        read(soa, stringa, sizeof(stringa));
-        printf("Stringa ricevuta: %s\n", stringa);
+        soa = accept(socketfd, (struct sockaddr*)&destination, &destlen);
+        
+        read(soa, message, sizeof(message));
+        write(soa, msg_received, sizeof(msg_received));
+
+        printf("Message received: %s\n", message);
         close(soa);
     }
 
+    close(socketfd);
+
+    printf("\n\n");
+    return 0;
 }
